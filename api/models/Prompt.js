6298ee714d0de77ae74d5d3a37453f5a71f6ad8e
Fawadpot <contactfawada@gmail.com>
2024-05-29 10:32:48 +0500

    added schema for prompt and promptgroup, added model methods for prompts, added routes for prompts

--- api/models/Prompt.js
+++ api/models/Prompt.js
@@ -1,39 +1,63 @@
-const mongoose = require('mongoose');
 const { logger } = require('~/config');
-
-const promptSchema = mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    prompt: {
-      type: String,
-      required: true,
-    },
-    category: {
-      type: String,
-    },
-  },
-  { timestamps: true },
-);
-
-const Prompt = mongoose.models.Prompt || mongoose.model('Prompt', promptSchema);
+const { ObjectId } = require('mongodb');
+const { Prompt, PromptGroup } = require('./schema/promptSchema');
 
 module.exports = {
-  savePrompt: async ({ title, prompt }) => {
+  savePrompt: async ({
+    prompt,
+    groupId,
+    type,
+    tags,
+    name,
+    isActive,
+    config,
+    labels,
+    author,
+    authorName,
+  }) => {
     try {
-      await Prompt.create({
-        title,
+      labels.push('latest');
+
+      // Check if the groupId exists
+      let promptGroupId = new ObjectId(groupId);
+      let versionNumber = 1;
+
+      if (!groupId) {
+        // If groupId does not exist, create a new PromptGroup
+        const newPromptGroup = await PromptGroup.create({ name, isActive });
+        promptGroupId = newPromptGroup._id;
+      } else {
+        // If groupId exists, remove the "latest" label from all other prompts in the group
+        await Prompt.updateMany({ groupId: promptGroupId }, { $pull: { labels: 'latest' } });
+
+        // Calculate the version number for the new prompt
+        const promptCount = await Prompt.countDocuments({ groupId: promptGroupId });
+        versionNumber = promptCount + 1;
+      }
+
+      // Create a new prompt
+      const newPrompt = await Prompt.create({
         prompt,
+        groupId: promptGroupId,
+        type,
+        tags,
+        config,
+        labels,
+        version: versionNumber,
+        author,
+        authorName,
       });
-      return { title, prompt };
+
+      return newPrompt;
     } catch (error) {
       logger.error('Error saving prompt', error);
       return { prompt: 'Error saving prompt' };
     }
   },
   getPrompts: async (filter) => {
+    if (filter.groupId) {
+      filter.groupId = new ObjectId(filter.groupId);
+    }
     try {
       return await Prompt.find(filter).lean();
     } catch (error) {
@@ -41,12 +65,73 @@ module.exports = {
       return { prompt: 'Error getting prompts' };
     }
   },
-  deletePrompts: async (filter) => {
+  getPrompt: async (filter) => {
     try {
-      return await Prompt.deleteMany(filter);
+      if (filter.groupId) {
+        filter.groupId = new ObjectId(filter.groupId);
+      }
+      return await Prompt.findOne(filter).lean();
     } catch (error) {
-      logger.error('Error deleting prompts', error);
-      return { prompt: 'Error deleting prompts' };
+      logger.error('Error getting prompt', error);
+      return { prompt: 'Error getting prompt' };
+    }
+  },
+  getPromptGroupsWithPrompts: async (filter) => {
+    try {
+      return await PromptGroup.findOne(filter)
+        .populate({
+          path: 'prompts',
+          select: '-_id -__v -user',
+        })
+        .select('-_id -__v -user')
+        .lean();
+    } catch (error) {
+      logger.error('Error getting prompt groups', error);
+      return { prompt: 'Error getting prompt groups' };
+    }
+  },
+  getPromptGroups: async (filter) => {
+    try {
+      const { pageNumber, pageSize } = filter;
+      delete filter.pageNumber;
+      delete filter.pageSize;
+      return await PromptGroup.find(filter)
+        .sort({ updatedAt: -1 })
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize)
+        .lean();
+    } catch (error) {
+      logger.error('Error getting prompt groups', error);
+      return { prompt: 'Error getting prompt groups' };
+    }
+  },
+  deletePrompt: async ({ promptId, author }) => {
+    try {
+      const prompt = await Prompt.findOne({ _id: promptId, author });
+      if (!prompt) {
+        throw new Error('Prompt not found');
+      }
+      const groupId = new ObjectId(prompt.groupId);
+
+      await Prompt.deleteOne({ _id: promptId });
+
+      const remainingPrompts = await Prompt.countDocuments({ groupId });
+      if (remainingPrompts === 0) {
+        await PromptGroup.deleteOne({ _id: groupId });
+      }
+
+      return { message: 'Prompt deleted successfully' };
+    } catch (error) {
+      logger.error('Error deleting prompt', error);
+      return { prompt: 'Error deleting prompt' };
+    }
+  },
+  updatePromptGroup: async (filter, data) => {
+    try {
+      return await PromptGroup.updateOne(filter, data);
+    } catch (error) {
+      logger.error('Error updating prompt group', error);
+      return { prompt: 'Error updating prompt group' };
     }
   },
 };
---
+++ api/models/schema/promptSchema.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const promptGroupSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  numberOfGenerations: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const PromptGroup = mongoose.model('PromptGroup', promptGroupSchema);
+
+const promptSchema = new Schema(
+  {
+    groupId: {
+      type: Schema.Types.ObjectId,
+      ref: 'PromptGroup',
+      required: true,
+      index: true,
+    },
+    version: {
+      type: Number,
+      required: true,
+    },
+    projectId: {
+      type: Schema.Types.ObjectId,
+      required: false,
+      default: null,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    authorName: {
+      type: String,
+      required: true,
+    },
+    prompt: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ['text', 'chat'],
+      required: true,
+    },
+    config: {
+      type: Schema.Types.Mixed,
+      default: {},
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+    labels: {
+      type: [String],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Ensure a combination of groupId and version is unique
+promptSchema.index({ groupId: 1, version: 1 }, { unique: true });
+
+const Prompt = mongoose.model('Prompt', promptSchema);
+
+module.exports = { Prompt, PromptGroup };
