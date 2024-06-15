cc004199d1a9c0b20aa680c7f0824faed073f5cd
Danny Avila <danny@librechat.ai>
2024-06-15 18:04:36 -0400

    chore: clear convo state on navigation to dashboard routes

--- client/src/routes/Layouts/Dashboard.tsx
+++ client/src/routes/Layouts/Dashboard.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { QueryKeys } from 'librechat-data-provider';
+import { useQueryClient } from '@tanstack/react-query';
 import { useAuthContext } from '~/hooks';
+import store from '~/store';
 
 export default function DashboardRoute() {
+  const queryClient = useQueryClient();
+  const clearConvoState = store.useClearConvoState();
+  useEffect(() => {
+    queryClient.removeQueries([QueryKeys.messages, 'new']);
+    clearConvoState();
+  }, [queryClient, clearConvoState]);
   const { isAuthenticated } = useAuthContext();
   if (!isAuthenticated) {
     return null;
