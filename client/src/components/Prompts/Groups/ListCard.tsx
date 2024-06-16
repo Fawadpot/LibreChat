c5fc33402f85e3bcea5acd6da7985769b25a38b4
Danny Avila <danny@librechat.ai>
2024-06-16 02:05:31 -0400

    style(ListCard): border styling

--- client/src/components/Prompts/Groups/ListCard.tsx
+++ client/src/components/Prompts/Groups/ListCard.tsx
@@ -16,8 +16,8 @@ export default function ListCard({
   return (
     <div
       onClick={onClick}
-      className="relative my-2 flex w-full cursor-pointer flex-col gap-2 rounded-2xl border px-3 pb-4 pt-3 text-start
-      align-top text-[15px] shadow-[0_0_2px_0_rgba(0,0,0,0.05),0_4px_6px_0_rgba(0,0,0,0.02)] transition-all duration-300 ease-in-out hover:bg-gray-100 dark:hover:bg-gray-700"
+      className="relative my-2 flex w-full cursor-pointer flex-col gap-2 rounded-2xl border px-3 pb-4 pt-3 text-start align-top
+      text-[15px] shadow-[0_0_2px_0_rgba(0,0,0,0.05),0_4px_6px_0_rgba(0,0,0,0.02)] transition-all duration-300 ease-in-out hover:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-700"
     >
       <div className="flex w-full justify-between">
         <div className="flex flex-row gap-2">
