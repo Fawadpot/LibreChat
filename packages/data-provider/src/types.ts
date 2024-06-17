724579f5c4e1162b7377b301363f9001a9a7a729
Danny Avila <danny@librechat.ai>
2024-06-17 09:04:42 -0400

    chore: styling and types

--- packages/data-provider/src/types.ts
+++ packages/data-provider/src/types.ts
@@ -350,7 +350,7 @@ export type TPromptGroup = {
   numberOfGenerations?: number;
   oneliner?: string;
   category?: string;
-  projectId?: string | null;
+  projectIds?: string[];
   productionId?: string | null;
   productionPrompt?: Pick<TPrompt, 'prompt'> | null;
   author: string;
@@ -397,10 +397,7 @@ export type TCreatePromptResponse = {
   group?: TPromptGroup;
 };
 
-export type TUpdatePromptGroupPayload = {
-  name: string;
-  category?: string;
-};
+export type TUpdatePromptGroupPayload = Partial<TPromptGroup>;
 
 export type TUpdatePromptGroupVariables = {
   id: string;
